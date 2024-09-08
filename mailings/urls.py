99c15b08f8a4b3ba0cf2742b973dee20d52339ca
Efim Kashapov <bambalabes@gmail.com>
2024-09-08 23:14:15 +0500

    Добавлена страница с информацией по конкретной рассылке

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from mailings.apps import MailingsConfig
+from mailings.views import MailingsListView, MailingsDetailView
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
-
+    path('', MailingsListView.as_view(), name='mailings_list'),
+    path('<int:pk>/', MailingsDetailView.as_view(), name='mailings_view'),
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from mailings.models import Mailings
 
 
 # Create your views here.
 class MailingsListView(ListView):
+    model = Mailings
+
+
+class MailingsDetailView(DetailView):
     model = Mailings
