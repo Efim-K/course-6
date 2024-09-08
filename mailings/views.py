fab61f0b966436b053940938532701062a742b01
Efim Kashapov <bambalabes@gmail.com>
2024-09-08 20:43:01 +0500

    Создание главной страницы

--- mailings/views.py
+++ mailings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from mailings.models import Mailings
+
 
 # Create your views here.
+class MailingsListView(ListView):
+    model = Mailings
