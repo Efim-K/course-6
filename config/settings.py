cca6d62ba9fe0f36d14e6a171b5324d77345a45e
Efim Kashapov <bambalabes@gmail.com>
2024-09-08 15:52:06 +0500

    исправлено имя доступа к базе

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "homework20",
+        "NAME": "mailings",
         "USER": "postgres",
         "PASSWORD": "Q12345!",
         "HOST": "127.0.0.1",
