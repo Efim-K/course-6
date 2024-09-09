0159d1f3776fb04c964fd89f0338ba8b47baa4df
Efim Kashapov <bambalabes@gmail.com>
2024-09-09 23:23:26 +0500

    Созданы разделы по созданию редактированию удалению рассылок

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
 from mailings.apps import MailingsConfig
-from mailings.views import MailingsListView, MailingsDetailView
+from mailings.views import MailingsListView, MailingsDetailView, MailingsCreateView, MailingsUpdateView, \
+    MailingsDeleteView
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
     path('', MailingsListView.as_view(), name='mailings_list'),
     path('<int:pk>/', MailingsDetailView.as_view(), name='mailings_view'),
+    path('create/', MailingsCreateView.as_view(), name='mailings_create'),
+    path('<int:pk>/update/', MailingsUpdateView.as_view(), name='mailings_update'),
+    path('<int:pk>/delete/', MailingsDeleteView.as_view(), name='mailings_delete'),
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,13 +1,49 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from mailings.models import Mailings
 
 
 # Create your views here.
 class MailingsListView(ListView):
+    """
+    Выводит список всех рассылок
+    """
     model = Mailings
 
 
 class MailingsDetailView(DetailView):
+    """
+    Выводит детальную информацию о рассылке
+    """
     model = Mailings
+
+
+class MailingsCreateView(CreateView):
+    model = Mailings
+    """
+    Создает новую рассылку
+    """
+    fields = ('launch_at', 'completed_at', 'periodicity', 'status', 'message', 'email_client')
+    success_url = reverse_lazy('mailings:mailings_list')
+
+
+class MailingsUpdateView(UpdateView):
+    """
+    Редактирует существующую рассылку
+    """
+    model = Mailings
+    fields = ('launch_at', 'completed_at', 'periodicity', 'status', 'message', 'email_client')
+    success_url = reverse_lazy('mailings:mailings_list')
+
+    def get_success_url(self):
+        return reverse_lazy('mailings:mailings_view', args=[self.object.pk])
+
+
+class MailingsDeleteView(DeleteView):
+    """
+    Удаляет существующую рассылку
+    """
+    model = Mailings
+    success_url = reverse_lazy('mailings:mailings_list')
